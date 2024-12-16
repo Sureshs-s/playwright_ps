f6962014bf9ac5f65a0b131927ccb09c1ad825db
Suresh-s <jawasuresh22@gmail.com>
2024-12-17 00:23:33 +0530

    `Updated playwright.config.js and tests to reflect changes in browser settings and test cases`

---
+++ tests/dropdown.spec.js
@@ -0,0 +1,67 @@
+/*const { test, expect } = require('@playwright/test');
+
+test.only('Multiple dropdown', async ({ page }) => {
+  const browser = await chromium.launch({ headless: false });
+
+  await page.goto('https://testautomationpractice.blogspot.com/');
+  await page.fill("//input[@id='name']",'Suresh');
+  await page.waitForTimeout(2000);
+  await page.fill("//input[@id='email']",'Suresh@gmail.com');
+  await page.waitForTimeout(2000);
+  await page.fill("//input[@id='phone']",'8438556629');
+  await page.check("//input[@id='male']");
+  await page.waitForTimeout(2000);
+  await page.click("//input[@id='monday']");
+  await page.click("//input[@id='saturday']");
+  await page.waitForTimeout(2000);
+  await page.click("//input[@id='tuesday']");
+
+  await page.waitForTimeout(4000);
+  await page.screenshot({ path: 'testautomation screenshot.png' })
+  //await page.screenshot()
+  //Dropdown
+
+  await page.selectOption("//select[@id='country']",'france');
+await page.waitForTimeout(4000);
+  await page.selectOption("//select[@id='country']",'China');
+  
+  const dropdown = page.locator("//select[@id='country']");
+  const options = page.locator(options).allTextContents();
+
+  console.log(dropdown);
+//const dropdownlist = await page.selectOption("//select[@id='country']")
+
+await browser.close();
+});*/
+
+
+const { test, expect } = require('@playwright/test');
+/*const browser = await chromium.launch({ headless: false });
+const page = await browser.newPage()*/
+
+
+/*const { test, expect } = require('@playwright/test');
+
+test.only('Multiple dropdown', async ({ page }) => {
+  const browser = await chromium.launch({ headless: false });*/
+
+test('checking dropdown',async ({page}) => {
+  
+  // Navigate to the Booking.com search results page
+  const url = 'https://www.booking.com/searchresults.html?ss=New+York%2C+New+York%2C+United+States&efdco=1&label=gen173bo-1DCAEoggI46AdIM1gDaGyIAQGYATG4ARfIAQzYAQPoAQH4AQKIAgGYAgKoAgO4Avq35roGwAIB0gIkYzliNWM3MDItYmI4YS00MzViLWJjMGMtMDJkYjExYzVmYjM22AIE4AIB&aid=304142&lang=en-us&sb=1&src_elem=sb&src=index&dest_id=20088325&dest_type=city&ac_position=0&ac_click_type=b&ac_langcode=en&ac_suggestion_list_length=5&search_selected=true&search_pageview_id=2b2262fde14a00b9&ac_meta=GhAyYjIyNjJmZGUxNGEwMGI5IAAoATICZW46B25ld3lvcmtAAEoAUAA%3D&group_adults=2&no_rooms=1&group_children=0';
+  await page.goto(url);
+
+await page.click("//button[@aria-label='Dismiss sign-in info.']");
+
+  // Wait for the hotel list to load
+  ///await page.waitForSelector('.sr_property_block_main_row');
+
+  // Extract hotel names
+  const hotelNames = await page.locator('.sr_property_block_main_row .fcab3ed991.a23c043802').allTextContents();
+
+  // Print hotel names
+  console.log('Hotels found:');
+  hotelNames.forEach((name, index) => console.log(`${index + 1}. ${name}`));
+
+  await browser.close();
+});
--- tests/ticketsnew.spec.js
+++ tests/ticketsnew.spec.js
@@ -1,6 +1,6 @@
 const { test, expect } = require('@playwright/test');
 
-test.only('Bookmy show login page', async ({ page }) => {
+test.skip('Bookmy show login page', async ({ page }) => {
   await page.goto('https://ticketnew.com/');
 
   await page.locator('tagName=span');
@@ -16,13 +16,70 @@ test.only('Bookmy show login page', async ({ page }) => {
   await page.click('.Button_btn___t8GZ')
   await page.waitForTimeout(3000);
   
-  
+  await page.close();
 
   await page.screenshot({ path: 'screenshot1.png' });
-
   
 
 });
+
+test.skip ("assertion testing" ,async({page})=>{
+  
+await page.goto("https://demo.automationtesting.in/Register.html");
+
+await page.locator("//input[@placeholder='First Name']").fill("suresh");
+
+await page.locator("//input[@placeholder='Last Name']").fill("shankar");
+//await page.pause();
+await page.fill("//textarea[@ng-model='Adress']","144/abc street , thriyvanmuyur chennai -41");
+await page.fill("//input[@type='email']","slsp225@gmail");
+await page.fill("//input[@type='tel']","+918438556629");
+await page.click("//input[@ng-model='radiovalue']");
+
+
+
+await page.waitForTimeout(3000);
+ 
+await page.click("//input[@id='checkbox3']");
+
+await page.waitForTimeout(3000);
+
+await page.click("//input[@id='checkbox2']");
+ await page.locator('#msdd').click();
+
+ await page.click("//a[normalize-space()='Icelandic']");
+ await page.click("//a[normalize-space()='Italian']");
+ await page.click("//a[normalize-space()='Hindi']");
+ await page.click("//a[normalize-space()='Urdu']");
+
+ //await page.pause();
+
+ //await 
+
+// await page.locator('option[value="Hindi"]').click();
+
+//element.selectOption('blue');
+//input[@placeholder='Last Name']
+});
+
+test.only ("Dropdown2" ,async({page})=>{
+  
+  await page.goto("https://testautomationpractice.blogspot.com/");
+
+  const dropdownfile= await page.selectOption("//select[@id='country']/option").getByAltText();
+
+  console.log(dropdownfile);
+
+
+
+
+
+
+});
+
+
+
+
 
 /*test.only('dummy ticket new' , async({page})=>{
     await page.goto('https://ticketnew.com/');
